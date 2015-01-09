9a5fbe569dce48a3cf7c40e1522148e47dba3fac
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-01-10 00:54:36 +0200

    fix bug in adding ads

--- app/js/controllers/UserPublishNewAdController.js
+++ app/js/controllers/UserPublishNewAdController.js
@@ -10,13 +10,30 @@ app.controller('UserPublishNewAdController',
       $scope.publishAd = function(adData) {
           userService.createNewAd(adData,
               function success() {
-                  // TODO: show “success” info message
+                  // TODO: show ï¿½successï¿½ info message
                   $location.path("/user/ads");
               },
               function error(err) {
-                  // TODO: show error message
+                  console.log(err);
               }
           );
       };
+
+      $scope.fileSelected = function(fileInputField) {
+          delete $scope.adData.imageDataUrl;
+          var file = fileInputField.files[0];
+          if (file.type.match(/image\/.*/)) {
+              var reader = new FileReader();
+              reader.onload = function() {
+                  $scope.adData.imageDataUrl = reader.result;
+                  $(".image-box").html("<img src='" + reader.result + "'>");
+              };
+              reader.readAsDataURL(file);
+          } else {
+              $(".image-box").html("<p>File type not supported!</p>");
+          }
+      };
   }
 );
+
+
--- app/js/services/userService.js
+++ app/js/services/userService.js
@@ -3,32 +3,32 @@
 app.factory('userService',
     function ($http, baseServiceUrl, authService) {
         return {
-            createNewAd function (adData, success, error) {
+            createNewAd: function (adData, success, error) {
                 var request = {
-                    method 'POST',
-                    url baseServiceUrl + 'apiuserads',
-                    headers authService.getAuthHeaders(),
-                    data adData
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/user/ads',
+                    headers: authService.getAuthHeaders(),
+                    data: adData
                 };
                 $http(request).success(success).error(error);
             },
 
-            getUserAds function (params, success, error) {
+            getUserAds: function (params, success, error) {
                 var request = {
-                    method 'GET',
-                    url baseServiceUrl + 'apiuserads',
-                    headers authService.getAuthHeaders(),
-                    params params
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/user/ads',
+                    headers: authService.getAuthHeaders(),
+                    params: params
                 };
                 $http(request).success(success).error(error);
             },
 
-            deactivateAd function (id, success, error) {
-                 TODO
+            deactivateAd: function (id, success, error) {
+                // TODO
             },
 
-            publishAgainAd function (id, success, error) {
-                 TODO
+            publishAgainAd: function (id, success, error) {
+                 //TODO
             }
         }
     }
