4273dcbb4e9511ba9b32f98358296f02e18356fc
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-01-10 18:39:56 +0200

    add reference to DeleteAdController

--- app/js/controllers/UserAdsController.js
+++ app/js/controllers/UserAdsController.js
@@ -24,31 +24,5 @@ app.controller('UserAdsController',
         };
 
         $scope.reloadUserAds();
-
-        $scope.deactivateAd = function(adId) {
-            userService.deactivateAd(
-                adId,
-                function success(data) {
-                    notifyService.showInfo('Successfully deactivated ad');
-                    $scope.reloadAds();
-                },
-                function error(err) {
-                    notifyService.showError("There is a error", err);
-                }
-            );
-        };
-
-        $scope.publishAgainAd = function(adId) {
-            userService.publishAgainAd(
-                adId,
-                function success(data) {
-                    notifyService.showInfo('Successfully Publish ad');
-                    $scope.reloadAds();
-                },
-                function error(err) {
-                    notifyService.showError("There is a error", err);
-                }
-            );
-        };
    }
 );
