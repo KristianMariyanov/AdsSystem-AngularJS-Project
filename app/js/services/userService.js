67781055461c67a4763b409c1601254b0c1be083
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-01-10 16:22:37 +0200

    add method for activate and deactivate of ads

--- app/js/services/userService.js
+++ app/js/services/userService.js
@@ -24,12 +24,23 @@ app.factory('userService',
             },
 
             deactivateAd: function (id, success, error) {
-                // TODO
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/user/ads/deactivate/' + id,
+                    headers: authService.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
             },
 
             publishAgainAd: function (id, success, error) {
-                 //TODO
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/user/ads/publishAgain/' + id,
+                    headers: authService.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
             }
         }
     }
 );
+
