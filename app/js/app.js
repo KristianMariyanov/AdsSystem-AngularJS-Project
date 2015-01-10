77afae419ef27d4be690c23d23a0a8ea90c9ee45
Kristian Mariyanov <kristian.mariyanov@gmail.com>
2015-01-10 10:55:21 +0200

    add route for user ads

--- app/js/app.js
+++ app/js/app.js
@@ -27,6 +27,11 @@ app.config(function ($routeProvider) {
         controller: 'UserPublishNewAdController'
     });
 
+    $routeProvider.when('/user/ads', {
+        templateUrl: 'templates/user/user-ads.html',
+        controller: 'UserPublishNewAdController'
+    });
+
     $routeProvider.otherwise(
         { redirectTo: '/' }
     );
